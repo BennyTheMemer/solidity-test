583fd169aa5ea4a4ec2b9d2807dc213b1bdd378c
BennyTheMemer <bernardolcc98@gmail.com>
2022-01-19 06:12:37 +0000

    fix next.config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   reactStrictMode: true,
-}
+};
+const withTM = require("next-transpile-modules")(["react-hook-mousetrap"]);
+
+module.exports = withTM({
+  /* Your Next.js config */
+});
